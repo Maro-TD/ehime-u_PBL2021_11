8d9d98da12352f98aab7993b240cf54b6211c395
g520275x <91862113+Maro-TD@users.noreply.github.com>
2021-10-24 16:36:06 +0900

    Update image.cc

--- image.cc
+++ image.cc
@@ -339,7 +339,9 @@ void draw_detections_v3(image im, detection *dets, int num, float thresh, char *
     int i;
     for (i = 0; i < selected_detections_num; ++i) {
         const int best_class = selected_detections[i].best_class;
-        printf("%s: %.0f%%", names[best_class],    selected_detections[i].det.prob[best_class] * 100);
+        if("%s",names[j] == person){
+            printf("%s: %.0f%%", names[best_class],    selected_detections[i].det.prob[best_class] * 100);
+        }
         if (ext_output)
             printf("\t(left_x: %4.0f   top_y: %4.0f   width: %4.0f   height: %4.0f)\n",
                 round((selected_detections[i].det.bbox.x - selected_detections[i].det.bbox.w / 2)*im.w),
