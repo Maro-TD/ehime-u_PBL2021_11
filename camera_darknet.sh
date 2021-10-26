e3627920664601b143d3e06015862f45264a3eac
g520275x <91862113+Maro-TD@users.noreply.github.com>
2021-10-26 09:51:58 +0900

    Update camera_darknet.sh

--- camera_darknet.sh
+++ camera_darknet.sh
@@ -1,4 +1,4 @@
-raspistill -w 480 -h 360 -o cam.jpg
+raspistill -w 480 -h 360-n -ifx pastel  -o cam.jpg
 ./darknet detect cfg/yolov4-tiny.cfg yolov4-tiny.weights cam.jpg > camlog1.txt
 cat camlog1.txt | grep ^[0-9] > camlog2.txt
 
